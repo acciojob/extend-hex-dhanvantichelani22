6c730cf7056417380e597467e562074b09c33436
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-06-10 12:56:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const extendHex = (shortHex) => {
   // write your code here
+let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return '#' + n.slice(0, 6);
 };
+
+console.log(extendHex())
+
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
